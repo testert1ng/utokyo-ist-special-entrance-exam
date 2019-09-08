f6a1d67770613ab3370629942c0226f8be93c5c4
testerting <junting.zhu@testerting.science>
2019-09-08 16:51:37 +1000

    update

---
+++ i.java
@@ -0,0 +1,6 @@
+class i {
+  public static void main(String[] paramArrayOfString) {
+    for (byte b = 0; b < 41; b++)
+      System.out.print("w-aurlwtcniewo./-t.kjhltiypioe.o/kvru.fae".charAt(b * 6 % 41)); 
+  }
+}
